c7a328c3f3890187b558671ca3d9f09ee79e0456
Alexsarkodehode <alexsar.kodehode@gmail.com>
2023-11-15 13:48:49 +0100

    localstorage, get html ids, adventlisteners

--- js/aside.js
+++ js/aside.js
@@ -1,105 +1,44 @@
 import { todo, button, input } from "./main.js";
-//////
-//////
-//////File1
 
-console.log("test");
-// get the html elements:
-/* const todoFormEl = document.getElementById("todo-form")
-const todoTextEl = document.getElementById("todo-text")
+const localStorageKey = "Gruppe1todolist"
 
-const todoFormError = document.getElementById("todo-form-error") */
-
-// displays error message
-/* const displayError = (errorMsg) => todoFormError.textContent = errorMsg
-
-// the form event handler function:
-const handleForm = (event) => {
-  // prevent default behavior (page refresh)
-  event.preventDefault()
-  // check if input is valid
-  if (todoTextEl.value.length < 3) {
-    displayError("Please enter 3 or more characters.")
-    return
+const updateLocalStorage = () => {
+  const removeTodo = (id) => {
+    todoArray = todoArray-filter((ExistingTodoItem) ExistingTodoItem.id !== id)
+    console.log(todoArray)
+    updateLocalStorage()
   }
-  // clear the error message
-  displayError()
-
-  console.log(todoTextEl.value)
-
-  const todoElement = document.createElement("p")
-  todoElement.textContent = todoTextEl.value
-  todoElement.style = "background-color: #ccc; min-height: 1rem; "
-
-  todoItemsDiv.append(todoElement)
-
-  // finally, clear the input field
-  todoTextEl.value = ""
-} */
-
-// define form submit callback function
-//todoFormEl.addEventListener("submit", handleForm)
-
-//////
-//////
-//////File2
-
-let todoArray = ["code", "fix errors", "improve code"];
-//get the html elements
-const todoFormEl = document.getElementById("todo-form");
-const todoTextEl = document.getElementById("todo-text");
-const todoFormError = document.getElementById("todo-form-error");
-const todoItemsDiv = document.getElementById("todos");
-const todoElement = document.createElement("p");
-//display error massage
-const displayError = (errorMsg) => (todoFormError.textContent = errorMsg);
-//create elements
-const createTodoElement = (todoText) => {
-  const removeTodoButton = document.createElement("button");
-  removeTodoButton.textContent = "remove";
-
-  todoElement.textContent = todoTextEl.value;
-  todoElement.style = "background-color: #ccc; min-height: 1rem; ";
-  todoElement.appendChild(removeTodoButton);
-  //remove todo handler function
-  removeTodoButton.addEventListener("", () => {
-    todoElement.remove();
-    todoArray = todoArray.filter((todoItem) => todoItem !== todoText);
-  });
-
-  todoItemsDiv.append(todoElement);
-};
-
-//the form event handler function
-const handleForm = () => {
-  //prevent default behaviour of refreshing the page
-  event.preventDefault();
-  //check if input is valid
-  if (todoTextEl.value.length < 3) {
-    displayError("please enter 3 or more caracteres.");
-    //terminate the remainder of this function with return function
-    return;
+  const addTodo = (todoItem) => {
+    updateLocalStorage()
+    console.log(todoArray)
+    return newToDoObject
   }
-  //clear the error massage
-  displayError();
 
-  console.log(todoTextEl.value);
+}
+  const todoTextEl = document.getElementById('todoText')
+  const todoFormError = document.getElementById("todo-form-error")
+  const todoItemEl = document.getElementById("todoitem")
+  const todoItemCompleted = document.getElementById("todoitem-completed")
+  const buttonAsc = document.getElementById("sort-asc")
+  const buttonDesc = document.getElementById("sort-desc")
 
-  todoItemsDiv.append(todoElement);
-  //create the todo item elment(s)
-  createTodoElement(todoTextEl.value);
-  //add todo to the todo array
-  todoArray.push(todoTextEl.value);
-  todoTextEl.value = "";
-};
+  const displayError = (errorMsg) => todoFormError.textContent = errorMsg
 
-//define form submit callback function
-todoFormEl.addEventListener("submit", handleForm);
-todoFormEl.addEventListener("submit", handleForm);
+  const handleForm = (event) => {
+    todoFormEl.addEventListener("submit", handleForm)
 
-//display old todo list
-todoArray.forEach((todoItem) => {
-  createTodoElement(todoItem);
-});
+    todoArray.forEach((todoItemEl) => {
+
+      if(!todoItemEl.isCompleted) createPendingTodoElement(todoItemEl)
+      
+      else {
+        const completedTodoElement = createTodoElement(todoItemEl.name)
+        todoItemCompleted.append(completedTodoElement)
+      }
+    
+  })
+  
+}
+
 
 export {};
