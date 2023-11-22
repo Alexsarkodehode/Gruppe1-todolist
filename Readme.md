dc187312a0407fe12360ce92782281575b5a1bdf
AndreK-B06 <andrekb6.kodehode@gmail.com>
2023-11-22 10:15:08 +0100

    Github link added in nav menu

--- Readme.md
+++ Readme.md
@@ -19,9 +19,10 @@ André:
     - helpt alex orgenise the team
       -change folder text document and made easy for grupe work.
     - helpt maid projekt overvue.
-      -morkt on inport and export.
-      -helpd on understanding new files.
+      -workt on inport and export.
+      -helpd the team understanding the new files and folders.
       -added temproary aside.js inn html.
       -Added animation to the nav menu.
-      - maid nav menu
-        -problem fixing nav menu after inplementing others code.
+      - created nav menu
+        -Fixst nav menu problem after inplementing grupe css code.
+        -Linkt Github link to all devs github inn nav menu.
--- js/header.js
+++ js/header.js
@@ -7,3 +7,40 @@ navBtnEl.addEventListener("click", function () {
   navBtnEl.classList.toggle("is-active");
   navbarEl.classList.toggle("is-active");
 });
+/* ----------------------------
+!!!! Link to Deve's Github !!!!
+----------------------------- */
+const alexanderGithub = document.querySelector("#alexander");
+const andreGithub = document.querySelector("#andre");
+const ilakiaGithub = document.querySelector("#ilakia");
+
+alexanderGithub.addEventListener("click", function () {
+  window.open("https://github.com/Alexsarkodehode", "_blank");
+});
+
+andreGithub.addEventListener("click", function () {
+  window.open("https://github.com/AndreK-B06", "_blank");
+});
+ilakiaGithub.addEventListener("click", function () {
+  window.open("https://github.com/Ilakiab08", "_blanck");
+});
+
+// const githubBtn = {
+//   alex: query,
+//   andre: query,
+//   ilakia: query,
+// };
+// const githubLink = {
+//   alex: link,
+//   andre: link,
+//   ilakia: link,
+// };
+// githubBtn.alex.addEventListener("click", function () {
+//   window.open(githubLink.alex);
+// });
+// githubBtn.andre.addEventListener("click", function () {
+//   window.open(githubLink.andre);
+// });
+// githubBtn.ilakia.addEventListener("click", function () {
+//   window.open(githubLink.ilakia);
+// });
